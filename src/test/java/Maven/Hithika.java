d4abf2f9abdedf8dea7d8fded12b437479c71fe8
Mendepoojitha123 <mendepoojitha@gmail.com>
2023-12-06 16:40:38 +0530

    i have added one class

---
+++ src/test/java/Maven/Hithika.java
@@ -0,0 +1,11 @@
+package Maven;
+
+public class Hithika {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi hithika papa how are you");
+
+	}
+
+}
